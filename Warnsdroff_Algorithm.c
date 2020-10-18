2e7e857cf137070f2c1d02913fdb3ed2186e2591
Rajdeepsavani <56822565+savanirajdeep@users.noreply.github.com>
2020-10-18 23:30:21 +0530

    Create Warnsdroff_Algorithm.c

---
+++ Warnsdroff_Algorithm.c
@@ -0,0 +1,144 @@
+/*  
+*   @author Savani Rajdeep
+*   @desc Program to Warnsdroff algorithm
+*/
+
+#include<stdio.h>
+
+int n,x,y,i,M[1000000],G[1000][1000];
+int ans[10][10],counter=0;
+
+
+void jump(int d)
+{
+	if(M[i]==0)
+	{
+		x = x - 1*d;
+		y = y + 2*d;
+	}
+	else if(M[i]==1)
+	{
+		x = x - 2*d;
+		y = y + 1*d;
+	}
+	else if(M[i]==2)
+	{
+		x = x - 2*d;
+		y = y - 1*d;
+	}
+	else if(M[i]==3)
+	{
+		x = x - 1*d;
+		y = y - 2*d;
+	}
+	else if(M[i]==4)
+	{
+		x = x + 1*d;
+		y = y - 2*d;
+	}
+	else if(M[i]==5)
+	{
+		x = x + 2*d;
+		y = y - 1*d;
+	}
+	else if(M[i]==6)
+	{
+		x = x + 2*d;
+		y = y + 1*d;
+	}
+	else if(M[i]==7)
+	{
+		x = x + 1*d;
+		y = y + 2*d;
+	}
+}
+//min degree
+int count()
+{
+	int count=0;
+	if((x-1)>=0 && (y+2)<n && G[x-1][y+2]==-1)
+		count++;
+	if((x-2)>=0 && (y+1)<n && G[x-2][y+1]==-1)
+		count++;
+	if((x-2)>=0 && (y-1)>=0 && G[x-2][y-1]==-1)
+		count++;
+	if((x-1)>=0 && (y-2)>=0 && G[x-1][y-2]==-1)
+		count++;
+	if((x+1)<n && (y-2)>=0 && G[x+1][y-2]==-1)
+		count++;
+	if((x+2)<n && (y-1)>=0 && G[x+2][y-1]==-1)
+		count++;
+	if((x+2)<n && (y+1)<n && G[x+2][y+1]==-1)
+		count++;
+	if((x+1)<n && (y+2)<n && G[x+1][y+2]==-1)
+		count++;
+	return count;	
+}
+
+int main()
+{
+	int x1,y1,c,d,min=8;
+	
+	printf("Enter size of board : ");
+	scanf("%d",&n);
+	
+	//initialise
+	for(x=0;x<n;x++)
+		for(y=0;y<n;y++)
+			G[x][y] = -1;
+			
+	//initial position
+	printf("Enter starting position (0-%d)\n",n-1);
+	printf("Row : ");
+	scanf("%d",&x);
+	printf("Col : ");
+	scanf("%d",&y);
+	
+	x1=x; y1=y;
+
+	//Knight's tour
+	for(i=1;;i++)
+	{
+		G[x][y]=i;
+		min=8;
+		d=0;
+		for(M[i]=0;M[i]<8;M[i]++)
+		{
+			jump(1);
+			if(x>=0 && x<n && y>=0 && y<n && G[x][y]==-1)
+			{
+				c=count();
+				if(c<min)
+				{
+					min=count();
+					d=M[i];
+				}
+			}
+			jump(-1);	
+		}
+		
+		if(min==8)
+			break;
+		
+		M[i]=d;
+		jump(1);
+
+		x1=x; y1=y;
+	}
+	
+	if(i==n*n)
+	{
+		printf("\nsolution found\n");
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<n;j++)
+			{
+				printf("%d\t",G[i][j]);
+			}
+			printf("\n");
+		}
+	}
+	else
+		printf("\nstuck... Can not find the solution.\n");
+	return 0;
+}
